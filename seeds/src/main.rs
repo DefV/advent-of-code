dd0beb1b4e851a7d884d1dc274d7e5597c46e82d
Jan De Poorter <jan@defv.be>
2023-12-05 16:37:14 +0100

    Day 5 - Part #2

--- seeds/src/main.rs
+++ seeds/src/main.rs
@@ -1,4 +1,8 @@
 use std::collections::HashMap;
+use rayon::prelude::*;
+
+pub mod range_map;
+use range_map::RangeMap;
 
 fn main() {
     // Open file passed in ARGV
@@ -14,46 +18,75 @@ fn main() {
         std::fs::read_to_string(filename).expect("Something went wrong reading the file");
 
     let (seeds, maps) = parse_document(document.as_str());
-    let min = seeds.iter().map(|i| maps.get("seed-to-soil").unwrap().get(*i) )
-                .map(|i| maps.get("soil-to-fertilizer").unwrap().get(i) )
-                .map(|i| maps.get("fertilizer-to-water").unwrap().get(i) )
-                .map(|i| maps.get("water-to-light").unwrap().get(i) )
-                .map(|i| maps.get("light-to-temperature").unwrap().get(i) )
-                .map(|i| maps.get("temperature-to-humidity").unwrap().get(i) )
-                .map(|i| maps.get("humidity-to-location").unwrap().get(i) )
-                .min().unwrap();
+    let min = parallel_find_min(seeds, maps);
 
     println!("Min: {}", min);
 }
 
-struct PointedRange {
-    source_range_start: u64,
-    destination_range_start: u64,
-    range_length: u64,
+struct Seeds {
+    seed_rules: Vec<(u64, u64)>,
 }
-struct RangeMap {
-    ranges: Vec<PointedRange>,
+struct SeedGenerator<'a> {
+    seeds: &'a Seeds,
+    rule_index: usize,
+    last: u64,
 }
 
-impl RangeMap {
-    fn get(&self, source: u64) -> u64 {
-        let mut destination = source;
 
-        for range in &self.ranges {
-            if source >= range.source_range_start
-                && source < range.source_range_start + range.range_length
-            {
-                destination = range.destination_range_start + (source - range.source_range_start);
-                break;
+impl<'a> Iterator for SeedGenerator<'a> {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let seed_rule = self.seeds.seed_rules[self.rule_index];
+        let next = seed_rule.0 + self.last;
+
+        if self.last >= seed_rule.1 {
+            if self.rule_index >= self.seeds.seed_rules.len() - 1 {
+                return None;
             }
-        }
 
-        destination
+            self.rule_index += 1;
+            self.last = 0;
+            println!("Incrementing rule index ({} / {})", self.rule_index + 1, self.seeds.seed_rules.len());
+        } else {
+            self.last += 1;
+        }
+        Some(next)
     }
 }
 
-fn parse_document(document: &str) -> (Vec<u64>, HashMap<&str, RangeMap>) {
-    let mut seeds: Vec<u64> = vec![];
+impl Seeds {
+    pub fn iter(&self) -> SeedGenerator {
+        SeedGenerator {
+            seeds: self,
+            rule_index: 0,
+            last: 0,
+        }
+    }
+}
+
+fn parallel_find_min(seeds: Seeds, maps: HashMap<&str, RangeMap>) -> u64 {
+    seeds.iter()
+        .par_bridge()
+        .map(|i| resolve_seed_location(i, &maps))
+        .min()
+        .unwrap()
+}
+
+fn resolve_seed_location(seed: u64, maps: &HashMap<&str, RangeMap>) -> u64 {
+    let mut i = seed;
+    i = maps.get("seed-to-soil").unwrap().get(i);
+    i = maps.get("soil-to-fertilizer").unwrap().get(i);
+    i = maps.get("fertilizer-to-water").unwrap().get(i);
+    i = maps.get("water-to-light").unwrap().get(i);
+    i = maps.get("light-to-temperature").unwrap().get(i);
+    i = maps.get("temperature-to-humidity").unwrap().get(i);
+    i = maps.get("humidity-to-location").unwrap().get(i);
+    i
+}
+
+fn parse_document(document: &str) -> (Seeds, HashMap<&str, RangeMap>) {
+    let mut seeds: Seeds = Seeds { seed_rules: vec![] };
     let mut maps: HashMap<&str, RangeMap> = HashMap::new();
 
     let mut map_buffer = String::new();
@@ -61,17 +94,12 @@ fn parse_document(document: &str) -> (Vec<u64>, HashMap<&str, RangeMap>) {
 
     for line in document.lines() {
         if line.contains("seeds: ") {
-            seeds = line
-                .split_whitespace()
-                .skip(1)
-                .map(str::parse::<u64>)
-                .flat_map(Result::ok)
-                .collect();
+            seeds = generate_seeds(line.trim_start_matches("seeds: "));
         } else if line.contains("map:") {
             map_name = line.split_whitespace().next().unwrap()
         } else if line == "" {
             if map_buffer.len() > 0 {
-                maps.insert(map_name, generate_map(map_buffer.as_str()));
+                maps.insert(map_name, RangeMap::from_str(map_buffer.as_str()));
                 map_buffer = String::new();
             }
         } else {
@@ -83,58 +111,35 @@ fn parse_document(document: &str) -> (Vec<u64>, HashMap<&str, RangeMap>) {
     (seeds, maps)
 }
 
-fn generate_map(map_str: &str) -> RangeMap {
-    let mut ranges: Vec<PointedRange> = map_str
-        .lines()
-        .map(|line| {
-            let parts: Vec<u64> = line
-                .split_whitespace()
-                .map(str::parse::<u64>)
-                .flat_map(Result::ok)
-                .collect();
+fn generate_seeds(seed_str: &str) -> Seeds {
+    let mut seed_definitions = seed_str
+        .split_whitespace()
+        .map(str::parse::<u64>)
+        .flat_map(Result::ok);
+    let mut seed_rules: Vec<(u64, u64)> = vec![];
 
-            PointedRange {
-                destination_range_start: parts[0],
-                source_range_start: parts[1],
-                range_length: parts[2],
-            }
-        })
-        .collect();
+    while let Some(start) = seed_definitions.next() {
+        let Some(count) = seed_definitions.next() else {
+            panic!("Nope")
+        };
 
-    ranges.sort_by_key(|range| range.source_range_start);
+        seed_rules.push((start, count));
+    }
 
-    RangeMap { ranges }
+    Seeds { seed_rules }
 }
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_generate_map() {
-        let input = "50 98 2\n52 50 48";
-        let map = generate_map(input);
 
-        assert_eq!(map.ranges.len(), 2);
-        assert_eq!(map.ranges[0].source_range_start, 50);
-        assert_eq!(map.ranges[0].destination_range_start, 52);
-        assert_eq!(map.ranges[0].range_length, 48);
-    }
-
-    #[test]
-    fn test_rangemap_get() {
-        let input = "50 98 2\n52 50 48";
-        let map = generate_map(input);
-
-        assert_eq!(map.get(14), 14);
-        assert_eq!(map.get(79), 81);
-        assert_eq!(map.get(97), 99);
-        assert_eq!(map.get(99), 51);
-    }
 
     #[test]
     fn test_parse_document() {
-        let input = "seeds: 79 14 55
+        let input = "seeds: 79 14 55 13
 
 test-one map:
 50 98 2
@@ -146,12 +151,7 @@ test-two map:
 
 ";
 
-        let (seeds, maps) = parse_document(input);
-
-        assert_eq!(seeds.len(), 3);
-        assert_eq!(seeds[0], 79);
-        assert_eq!(seeds[1], 14);
-        assert_eq!(seeds[2], 55);
+        let (_seeds, maps) = parse_document(input);
 
         assert_eq!(maps.len(), 2);
         assert_eq!(maps.get("test-one").unwrap().get(14), 14);
---
+++ seeds/src/range_map.rs
@@ -0,0 +1,75 @@
+use std::ops::Range;
+#[derive(Debug)]
+struct PointedRange {
+    source_range: Range<u64>,
+    destination_offset: i64,
+}
+
+pub struct RangeMap {
+    ranges: Vec<PointedRange>,
+}
+
+impl RangeMap {
+    pub fn from_str(map_str: &str) -> Self {
+        let mut ranges: Vec<PointedRange> = map_str
+            .lines()
+            .map(|line| {
+                let parts: Vec<u64> = line
+                    .split_whitespace()
+                    .map(str::parse::<u64>)
+                    .flat_map(Result::ok)
+                    .collect();
+
+                let source_range: Range<u64> = Range {
+                    start: parts[1],
+                    end: parts[1] + parts[2],
+                };
+                let destination_offset: i64 = parts[0] as i64 - parts[1] as i64;
+
+                PointedRange {
+                    source_range,
+                    destination_offset,
+                }
+            })
+            .collect();
+
+        ranges.sort_by_key(|range| range.source_range.start);
+
+        Self { ranges }
+    }
+
+    pub fn get(&self, source: u64) -> u64 {
+        self.ranges
+            .iter()
+            .find(|range| range.source_range.contains(&source))
+            .and_then(|range| source.checked_add_signed(range.destination_offset))
+            .unwrap_or(source)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_rangemap_from_str() {
+        let input = "50 98 2\n52 50 48";
+        let map = RangeMap::from_str(input);
+
+        assert_eq!(map.ranges.len(), 2);
+        assert_eq!(map.ranges[0].destination_offset, 2);
+        assert_eq!(map.ranges[0].source_range.start, 50);
+        assert_eq!(map.ranges[0].source_range.end, 98);
+    }
+
+    #[test]
+    fn test_rangemap_get() {
+        let input = "50 98 2\n52 50 48";
+        let map = RangeMap::from_str(input);
+
+        assert_eq!(map.get(14), 14);
+        assert_eq!(map.get(79), 81);
+        assert_eq!(map.get(97), 99);
+        assert_eq!(map.get(99), 51);
+    }
+}
