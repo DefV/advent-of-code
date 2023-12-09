61787576111c32a08312abddb73661d9938167db
Jan De Poorter <jan@defv.be>
2023-12-10 00:29:35 +0100

    Solve Day 9 - Part 1 & 2 Turns out it's way easier if you don't optimize for part 2 before knowing what part 2 is...

--- day9-sand/src/main.rs
+++ day9-sand/src/main.rs
@@ -1,5 +1,3 @@
-use nalgebra::{DMatrix, DVector};
-
 fn main() {
     // Open file passed in ARGV
     let args: Vec<String> = std::env::args().collect();
@@ -17,61 +15,24 @@ fn main() {
         .lines()
         .map(str::split_whitespace)
         .map(|seq| seq.map(str::parse::<i64>).filter_map(Result::ok).collect() )
-        .map(|seq| { 
-            assert_eq!(solve_n_for_seq(&seq, seq.len() - 1), seq[seq.len() - 1]);
-            seq
-        })
-        .map(|seq: Vec<_>| dbg!(solve_n_for_seq(dbg!(&seq), seq.len())))
+        .map(|seq: Vec<_>| dbg!(solve_next_for_seq(dbg!(&seq))))
         .sum();
 
     println!("{}", sum)
 }
 
-fn solve_n_for_seq(sequence: &Vec<i64>, n: usize) -> i64 {
-    let polynomial = find_polynomial(sequence);
-    let mut result = 0.0;
-
-    for (i, coefficient) in polynomial.iter().enumerate() {
-        result += coefficient * (n as f64).powi(i as i32);
-    }
-
-    result.round() as i64
-}
-
-fn find_polynomial(sequence: &Vec<i64>) -> Vec<f64> {
-    let degree = dbg!(find_degree(sequence.clone()));
-    let sequence = sequence.iter().map(|&x| x as f64).collect::<Vec<f64>>();
-    let rows = generate_vandermonde(sequence.len(), degree);
-    let columns = DVector::from_column_slice(&sequence);
-
-    
-    let coefficients = rows.clone().svd(true, true).solve(&columns, 1e-12).unwrap();
-
-    coefficients.iter().map(|&x| x).collect::<Vec<f64>>()
-}
-
-fn generate_vandermonde(size: usize, degree: usize) -> DMatrix<f64> {
-    let mut x_poly = DMatrix::from_element(size, degree + 1, 1.0);
-
-    for i in 0..size {
-        for j in 1..=degree {
-            x_poly[(i, j)] = (i as f64).powf(j as f64);
-        }
-    }
-
-    x_poly
-}
-
-fn find_degree(mut sequence: Vec<i64>) -> usize {
-    let mut degree = 0;
+fn solve_next_for_seq(sequence: &Vec<i64>) -> i64 {
+    let mut sequence = sequence.clone();
+    sequence.reverse();
+    let mut result = *sequence.last().unwrap();
 
     while !sequence.iter().all(|&x| x == 0) {
         // Calculate the difference between each element
         sequence = sequence.windows(2).map(|w| &w[1] - &w[0]).collect();
-        degree += 1;
+        result += sequence.last().unwrap();
     }
 
-    degree - 1 // The last iteration will always be all 0s
+    result
 }
 
 #[cfg(test)]
@@ -83,16 +44,9 @@ mod tests {
     static SEQUENCE_3: [i64; 6] = [10, 13, 16, 21, 30, 45];
 
     #[test]
-    fn test_find_degree() {
-        assert_eq!(find_degree(Vec::from(SEQUENCE_1)), 1);
-        assert_eq!(find_degree(Vec::from(SEQUENCE_2)), 2);
-        assert_eq!(find_degree(Vec::from(SEQUENCE_3)), 3);
-    }
-
-    #[test]
-    fn test_solve_n_for_seq() {
-        assert_eq!(solve_n_for_seq(&Vec::from(SEQUENCE_1), 6), 18);
-        assert_eq!(solve_n_for_seq(&Vec::from(SEQUENCE_2), 6), 28);
-        assert_eq!(solve_n_for_seq(&Vec::from(SEQUENCE_3), 6), 68);
+    fn test_solve_next_for_seq() {
+        assert_eq!(solve_next_for_seq(&Vec::from(SEQUENCE_1)), 18);
+        assert_eq!(solve_next_for_seq(&Vec::from(SEQUENCE_2)), 28);
+        assert_eq!(solve_next_for_seq(&Vec::from(SEQUENCE_3)), 68);
     }
 }
