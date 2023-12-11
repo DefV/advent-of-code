52ba2748c7e84a951d00b917205e8a74929af156
Jan De Poorter <jan@defv.be>
2023-12-11 23:13:15 +0100

    Add support for bigger step sizes

--- day11-galaxies/src/main.rs
+++ day11-galaxies/src/main.rs
@@ -13,30 +13,90 @@ fn main() {
 
     let expanded_map = expand_map(&document);
     let galaxies = map_galaxies(&expanded_map);
-    let galaxy_pairs = combine_galaxies(&galaxies);
 
-    let galaxy_distance: usize = galaxy_pairs.into_iter().map(|(g1, g2)| g1.distance_to(g2) ).sum();
+    let universe = Universe {
+        map: expanded_map,
+        galaxies: galaxies,
+    };
+
+    let galaxy_pairs = combine_galaxies(&universe.galaxies);
+
+    let galaxy_distance: usize = galaxy_pairs
+        .into_iter()
+        .map(|(g1, g2)| universe.distance_to(g1, g2))
+        .sum();
 
     println!("{}", galaxy_distance);
 }
 
-static BLANK_SPACE: char = '.';
-static GALAXY: char = '#';
+const BLANK_SPACE: char = '.';
+const GALAXY: char = '#';
 
-type Map = Vec<Vec<char>>;
+// Part 1
+// const EXPANDED_STEP_SIZE: usize = 2;
+// Part 2 example
+// const EXPANDED_STEP_SIZE: usize = 10;
+// Part 2
+const EXPANDED_STEP_SIZE: usize = 1_000_000;
+
+
+#[derive(PartialEq, Debug,Copy, Clone)]
+enum Space {
+    Nothing,
+    ExpandedNothing,
+    Galaxy,
+}
+
+impl Space {
+    fn from_char(c: &char) -> Space {
+        match *c {
+            GALAXY => Space::Galaxy,
+            _ => Space::Nothing,
+        }
+    }
+}
+
+type Map = Vec<Vec<Space>>;
+
+#[derive(Debug)]
+struct Universe {
+    map: Map,
+    galaxies: Vec<Galaxy>,
+}
+
+impl Universe {
+    fn distance_to(&self, a: &Galaxy, b: &Galaxy) -> usize {
+        let mut position = a.position;
+        let mut distance_traveled = 0;
+
+        while position != b.position {
+            let (x, y) = position;
+
+            if x < b.position.0 {
+                position.0 += 1;
+            } else if x > b.position.0 {
+                position.0 -= 1;
+            } else if y < b.position.1 {
+                position.1 += 1;
+            } else if y > b.position.1 {
+                position.1 -= 1;
+            }
+
+            let step_size = match self.map[position.0 as usize][position.1 as usize] {
+                Space::ExpandedNothing => EXPANDED_STEP_SIZE,
+                _ => 1
+            };
+            distance_traveled += step_size;
+            // println!("Step to #{},{} found a {:?}", position.0, position.1, self.map[position.0 as usize][position.1 as usize]);
+        }
+
+        distance_traveled
+    }
+}
 
 #[derive(Debug)]
 struct Galaxy {
-    position: (i32, i32)
-}
-
-impl Galaxy {
-    fn distance_to(&self, other: &Galaxy) -> usize {
-        let (x1, y1) = self.position;
-        let (x2, y2) = other.position;
-
-        ((x1 - x2).abs() + (y1 - y2).abs()) as usize
-    }
+    position: (i32, i32),
 }
 
 fn is_blank_space(c: &char) -> bool {
@@ -44,30 +104,27 @@ fn is_blank_space(c: &char) -> bool {
 }
 
 fn expand_map(document: &str) -> Map {
-    let mut expanded_map = Vec::new();
+    let mut expanded_map: Map = Vec::new();
 
     // Expand height
     let map: Vec<Vec<char>> = document
         .lines()
         .map(|line| line.chars().collect())
         .collect();
-        
+
     for i in 0..map.len() {
         if map[i].iter().all(is_blank_space) {
-            expanded_map.push(map[i].clone());
-            expanded_map.push(map[i].clone());
+            expanded_map.push(map[i].iter().map(|_| Space::ExpandedNothing).collect());
         } else {
-            expanded_map.push(map[i].clone());
+            expanded_map.push(map[i].iter().map(Space::from_char).collect());
         }
     }
 
-    let mut shifted_idx = 0;
     for i in 0..map[0].len() {
         if map.iter().all(|line| is_blank_space(&line[i])) {
             for line in expanded_map.iter_mut() {
-                line.insert(i + shifted_idx, BLANK_SPACE);
+                line[i] = Space::ExpandedNothing;
             }
-            shifted_idx += 1
         }
     }
 
@@ -79,8 +136,10 @@ fn map_galaxies(map: &Map) -> Vec<Galaxy> {
 
     for i in 0..map.len() {
         for j in 0..map[i].len() {
-            if map[i][j] == GALAXY {
-                galaxies.push(Galaxy { position: (i as i32, j as i32) });
+            if map[i][j] == Space::Galaxy {
+                galaxies.push(Galaxy {
+                    position: (i as i32, j as i32),
+                });
             }
         }
     }
@@ -92,7 +151,7 @@ fn combine_galaxies(galaxies: &Vec<Galaxy>) -> Vec<(&Galaxy, &Galaxy)> {
     let mut galaxy_pairs = Vec::new();
 
     for i in 0..galaxies.len() {
-        for j in i+1..galaxies.len() {
+        for j in i + 1..galaxies.len() {
             galaxy_pairs.push((&galaxies[i], &galaxies[j]));
         }
     }
@@ -110,10 +169,24 @@ mod tests {
 ....
 ...#";
         let expected = vec![
-            vec!['.', '.', '#', '.', '.', '.'],
-            vec!['.', '.', '.', '.', '.', '.'],
-            vec!['.', '.', '.', '.', '.', '.'],
-            vec!['.', '.', '.', '.', '.', '#'],
+            vec![
+                Space::ExpandedNothing,
+                Space::Galaxy,
+                Space::ExpandedNothing,
+                Space::Nothing,
+            ],
+            vec![
+                Space::ExpandedNothing,
+                Space::ExpandedNothing,
+                Space::ExpandedNothing,
+                Space::ExpandedNothing,
+            ],
+            vec![
+                Space::ExpandedNothing,
+                Space::Nothing,
+                Space::ExpandedNothing,
+                Space::Galaxy,
+            ],
         ];
         assert_eq!(expand_map(input), expected);
     }
