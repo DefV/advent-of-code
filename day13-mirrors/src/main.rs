64ca16d1ce3e13235bfeb30b0164a097e2922b9e
Jan De Poorter <jan@defv.be>
2023-12-13 22:43:37 +0100

    Day 13 - Part 1 implementation

---
+++ day13-mirrors/src/main.rs
@@ -0,0 +1,31 @@
+pub mod sequence;
+
+use sequence::Pattern;
+use sequence::Reflection;
+
+fn main() {
+    let now = std::time::Instant::now();
+    // Open file passed in ARGV
+    let args: Vec<String> = std::env::args().collect();
+    // Print usage if no file is passed
+    if args.len() < 2 {
+        println!("Usage: {} <filename>", args[0]);
+        return;
+    }
+
+    let filename = &args[1];
+    let document =
+        std::fs::read_to_string(filename).expect("Something went wrong reading the file");
+    
+    let sum = document.split("\n\n").map(Pattern::from).map(|pattern| pattern.find_reflection()).fold(0, |acc, (middle, reflection)| {
+        if reflection == Reflection::Horizontal {
+            acc + middle * 100
+        } else {
+            acc + middle
+        }
+    });
+
+    println!("Sum: {}", sum);
+
+    println!("Runtime: {:?}", now.elapsed());
+}
---
+++ day13-mirrors/src/sequence.rs
@@ -0,0 +1,134 @@
+use core::panic;
+use std::collections::HashSet;
+
+pub type Sequence = u64;
+
+#[derive(Debug, PartialEq)]
+pub enum Reflection {
+    Horizontal,
+    Vertical
+}
+
+#[derive(Debug)]
+pub struct Pattern {
+    rows: Vec<Sequence>,
+    columns: Vec<Sequence>,
+}
+
+impl From<&str> for Pattern {
+    fn from(document: &str) -> Self {
+        let column_count = document.lines().next().unwrap().len();
+        let mut column_chars: Vec<Vec<char>> = vec![vec![]; column_count];
+
+        let rows = Box::new(document.lines().map(str::chars))
+            .inspect(|chars| { chars.clone().enumerate().for_each(|(idx, c)| column_chars[idx].push(c) ) })
+            .map(|chars| parse_sequence(chars))
+            .collect();
+
+        let columns = column_chars
+            .into_iter()
+            .map(|chars| parse_sequence(chars.into_iter()))
+            .collect();
+
+        Self { rows, columns }
+    }
+}
+
+fn parse_sequence<I>(iter: I) -> Sequence
+where
+    I: IntoIterator<Item = char>,
+{
+    let mut sequence = 0;
+
+    for c in iter {
+        let bit = match c {
+            '#' => 1,
+            '.' => 0,
+            _ => panic!("Not a known character"),
+        };
+
+        sequence = (sequence << 1) | bit;
+    }
+
+    sequence
+}
+
+// This logic needs some love, but it works for now
+fn find_reflection(sequences: &Vec<Sequence>) -> Option<usize> {
+    let mut sequences = sequences.clone();
+
+    while sequences.len() > 1 {
+        if sequences.iter().rev().copied().collect::<Vec<u64>>() == sequences {
+            return Some(sequences.len() / 2);
+        }
+        
+        sequences.pop();
+    }
+
+    None
+}
+
+impl Pattern {
+    pub fn find_reflection(&self) -> (usize, Reflection) {
+        if let Some(reflection) = find_reflection(&self.rows) {
+            (reflection, Reflection::Horizontal)
+        } else if let Some(reflection) = find_reflection(&self.columns) {
+            (reflection, Reflection::Vertical)
+        } else if let Some(reflection) = find_reflection(&self.rows.iter().rev().copied().collect()) {
+            (self.rows.len() - reflection, Reflection::Horizontal)
+        } else if let Some(reflection) = find_reflection(&self.columns.iter().rev().copied().collect()) {
+            (self.columns.len() - reflection, Reflection::Vertical)
+        } else {
+            dbg!(&self);
+            panic!("No reflection found");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const FIRST_PATTERN: &str =
+"#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.";
+
+    const SECOND_PATTERN: &str =
+"#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#";
+
+    #[test]
+    fn test_parse_sequence() {
+        let sequence = parse_sequence(vec!['.', '#', '.', '#'].into_iter());
+        assert_eq!(sequence, 0b0101);
+    }
+
+    #[test]
+    fn test_pattern_from_str() {
+        let pattern = Pattern::from(FIRST_PATTERN);
+        assert_eq!(pattern.rows.len(), 7);
+        assert_eq!(pattern.columns.len(), 9);
+
+        assert_eq!(pattern.rows[0], 0b101100110);
+        assert_eq!(pattern.columns[0], 0b1011001);
+    }
+
+    #[test]
+    fn test_find_middle() {
+        let pattern = Pattern::from(FIRST_PATTERN);
+        assert_eq!(pattern.find_reflection(), (5, Reflection::Vertical));
+
+        let pattern = Pattern::from(SECOND_PATTERN);
+        assert_eq!(pattern.find_reflection(), (4, Reflection::Horizontal));
+    }
+}
