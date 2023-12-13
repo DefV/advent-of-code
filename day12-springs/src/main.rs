0bfdef1cff1bc100cf7789e97407ba739ebea8d9
Jan De Poorter <jan@defv.be>
2023-12-13 15:17:29 +0100

    Day 12 - Full rework to handle part 2

--- day12-springs/src/main.rs
+++ day12-springs/src/main.rs
@@ -1,8 +1,22 @@
-#[derive(Debug, PartialEq,Clone)]
+use std::collections::HashMap;
+
+use rayon::prelude::*;
+
+#[derive(PartialEq, Eq, Hash, Clone)]
 enum Spring {
     Operational,
     Damaged,
     Unknown,
+}
+
+impl std::fmt::Debug for Spring {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Spring::Operational => write!(f, "."),
+            Spring::Damaged => write!(f, "#"),
+            Spring::Unknown => write!(f, "?"),
+        }
+    }
 }
 
 impl From<char> for Spring {
@@ -22,12 +36,15 @@ struct Row {
     expected_sizes: Vec<usize>,
 }
 
-impl From<&str> for Row {
-    fn from(str: &str) -> Self {
+impl From<String> for Row {
+    fn from(str: String) -> Self {
         let (pattern, expected_sizes) = str.split_once(" ").unwrap();
 
         let pattern: Vec<Spring> = pattern.chars().map(Spring::from).collect();
-        let expected_sizes: Vec<usize> = expected_sizes.split(",").map(|s| s.parse().unwrap()).collect();
+        let expected_sizes: Vec<usize> = expected_sizes
+            .split(",")
+            .map(|s| s.parse().unwrap())
+            .collect();
 
         Self {
             pattern,
@@ -36,69 +53,86 @@ impl From<&str> for Row {
     }
 }
 
-impl Row {
-    pub fn possible_options(&self) -> Vec<Vec<Spring>> {
-        let mut options = vec![];
-        let mut pattern = self.pattern.clone();
+fn valid_options_for(pattern: Vec<Spring>, expected_sizes: Vec<usize>, cache: &mut HashMap<(Vec<Spring>, Vec<usize>), u128>) -> u128 {
+    if let Some(result) = cache.get(&(pattern.clone(), expected_sizes.clone())) {
+        return *result;
+    }
 
-        while let Some(spring) = pattern.pop() {
-            match spring {
-                Spring::Unknown => {
-                    if options.is_empty() {
-                        options.push(vec![Spring::Operational.clone()]);
-                        options.push(vec![Spring::Damaged.clone()]);
-                    } else {
-                        let mut new_options = vec![];
-                        for option in options.iter_mut() {
-                            let mut new_option = option.clone();
-                            new_option.insert(0, Spring::Operational.clone());
-                            new_options.push(new_option);
+    let mut valid_options = 0;
 
-                            let mut new_option = option.clone();
-                            new_option.insert(0, Spring::Damaged.clone());
-                            new_options.push(new_option);
-                        }
-                        options = new_options;
-                    }
-                },
-                known_spring => {
-                    if options.is_empty() {
-                        options.push(vec![known_spring])
+    let mut pattern = pattern;
+    let mut expected_sizes = expected_sizes;
+
+    // Find the first unknown or operational spring index
+    let first_unknown_spring_index =
+        pattern.iter().position(|spring| *spring != Spring::Damaged);
+
+    match first_unknown_spring_index {
+        Some(index) if pattern[index] == Spring::Operational => {
+            let part = &pattern[..=index];
+            match part {
+                [.., Spring::Damaged, Spring::Operational] => {
+                    let damaged_springs = part
+                        .iter()
+                        .filter(|spring| **spring == Spring::Damaged)
+                        .count();
+                    if expected_sizes.len() == 0 {
+                        // All good
+                    } else if damaged_springs == expected_sizes[0] {
+                        // We can consume a part
+                        expected_sizes.remove(0);
+                        pattern = pattern[index + 1..].to_vec();
+
+                        valid_options +=
+                            valid_options_for(pattern.clone(), expected_sizes.clone(), cache);
                     } else {
-                        for option in options.iter_mut() {
-                            option.insert(0, known_spring.clone());
-                        }
+                        // All good
                     }
+                }
+                _ => {
+                    pattern = pattern[index + 1..].to_vec();
+                    valid_options +=
+                        valid_options_for(pattern.clone(), expected_sizes.clone(), cache);
                 }
             }
         }
+        Some(index) => {
+            pattern[index] = Spring::Operational;
+            valid_options += valid_options_for(pattern.clone(), expected_sizes.clone(), cache);
 
-        options
-    }
+            pattern[index] = Spring::Damaged;
+            valid_options += valid_options_for(pattern.clone(), expected_sizes.clone(), cache);
+        }
+        None => {
+            let damaged_spring_count = pattern
+            .iter()
+            .filter(|&spring| *spring == Spring::Damaged)
+            .count();
 
-    pub fn valid_options(&self) -> usize {
-        self.possible_options().into_iter().filter(|option| self.is_valid(option)).count()
-    }
-
-    fn is_valid(&self, option: &Vec<Spring>) -> bool {
-        let mut counts: Vec<usize> = vec![];
-        let mut counter = 0;
-
-        for spring in option {
-            match spring {
-                Spring::Operational => { counts.push(counter); counter = 0 }
-                Spring::Damaged => counter += 1,
-                _ => panic!("Unknown spring in option!"),
+            if expected_sizes.len() == 1 && expected_sizes[0] == damaged_spring_count
+            {
+                valid_options += 1;
+            } else if expected_sizes.is_empty() && damaged_spring_count == 0 {
+                valid_options += 1;
+            } else {
+                // Fine
             }
         }
+    }
 
-        counts.push(counter);
-        counts = counts.into_iter().filter(|count| *count > 0).collect();
-        counts == self.expected_sizes
+    cache.insert((pattern, expected_sizes), valid_options);
+    valid_options
+}
+
+impl Row {
+    pub fn valid_options(&self) -> u128 {
+        let mut cache = HashMap::new();
+        valid_options_for(self.pattern.clone(), self.expected_sizes.clone(), &mut cache)
     }
 }
 
 fn main() {
+    let now = std::time::Instant::now();
     // Open file passed in ARGV
     let args: Vec<String> = std::env::args().collect();
     // Print usage if no file is passed
@@ -110,10 +144,20 @@ fn main() {
     let filename = &args[1];
     let document =
         std::fs::read_to_string(filename).expect("Something went wrong reading the file");
-    
-    let sum = document.lines().map(Row::from).map(|row| row.valid_options()).sum::<usize>();
-    
+
+    let sum = document
+        .lines()
+        .map(|line| {
+            let (pattern, sizes) = line.split_once(" ").unwrap();
+            vec![vec![pattern, pattern, pattern, pattern, pattern].join("?"), vec![sizes, sizes, sizes, sizes, sizes].join(",")].join(" ")
+        })
+        .map(Row::from)
+        .par_bridge()
+        .map(|row| dbg!(row.valid_options()))
+        .sum::<u128>();
+
     println!("Sum: {}", sum);
+    println!("Runtime: {:?}", now.elapsed());
 }
 
 #[cfg(test)]
@@ -131,7 +175,7 @@ mod tests {
 
     #[test]
     fn test_row_from_str() {
-        let row = Row::from("??..# 1,1");
+        let row = Row::from("??..# 1,1".to_string());
         assert_eq!(
             row,
             Row {
@@ -148,26 +192,14 @@ mod tests {
     }
 
     #[test]
-    fn test_spring_possible_options() {
-        let row = Row::from("??..# 1,1");
-        assert_eq!(
-            row.possible_options().len(), 4
-        );
-
-        assert_eq!(
-            row.possible_options(),
-            vec![
-                vec![Spring::Operational, Spring::Operational, Spring::Operational, Spring::Operational, Spring::Damaged],
-                vec![Spring::Damaged, Spring::Operational, Spring::Operational, Spring::Operational, Spring::Damaged],
-                vec![Spring::Operational, Spring::Damaged, Spring::Operational, Spring::Operational, Spring::Damaged],
-                vec![Spring::Damaged, Spring::Damaged, Spring::Operational, Spring::Operational, Spring::Damaged],
-            ]
-        )
+    fn test_spring_valid_options() {
+        assert_eq!(Row::from("??..# 1,1".to_string()).valid_options(), 2);
+        assert_eq!(Row::from("?###???????? 3,2,1".to_string()).valid_options(), 10);
     }
 
     #[test]
-    fn test_spring_valid_options() {
-        assert_eq!(Row::from("??..# 1,1").valid_options(), 2);
-        assert_eq!(Row::from("?###???????? 3,2,1").valid_options(), 10);
+    fn test_performance_of_big_expansion() {
+        let row = Row::from(".?.??????????#????#?.?.??????????#????#?.?.??????????#????#?.?.??????????#????#?.?.??????????#????#? 1,1,2,7,2,1,1,2,7,2,1,1,2,7,2,1,1,2,7,2,1,1,2,7,2".to_string());
+        assert_eq!(row.valid_options(), 2);
     }
 }
