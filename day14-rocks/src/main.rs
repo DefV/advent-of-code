d4ace06055847dfddbfa3e41a2fc01f30fd91dc2
Jan De Poorter <jan@defv.be>
2023-12-14 23:32:49 +0100

    Day 14 - Part 2
    Loopy...

--- day14-rocks/src/main.rs
+++ day14-rocks/src/main.rs
@@ -1,52 +1,85 @@
-#[derive(Clone,Copy,Debug)]
+use std::collections::{HashMap, hash_map::DefaultHasher};
+use std::hash::{Hash, Hasher};
+
+#[derive(Clone, Copy, Debug, Hash, PartialEq, Eq)]
 enum Rock {
     Round,
     Cube,
-    None
+    None,
 }
 
 type Platform = Vec<Vec<Rock>>;
 
 fn parse_platform(platform: &str) -> Platform {
-    let mut area: Platform = vec![vec![]; platform.split_once("\n").unwrap().0.len()];
-
-    for line in platform.lines() {
-        for (i, c) in line.chars().enumerate() {
-            match c {
-                'o' | 'O' => area[i].push(Rock::Round),
-                '#' => area[i].push(Rock::Cube),
-                _ => area[i].push(Rock::None)
-            }
-        }
-    }
-
-    area
+    platform
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| match c {
+                    'o' | 'O' => Rock::Round,
+                    '#' => Rock::Cube,
+                    '.' => Rock::None,
+                    _ => unreachable!(),
+                })
+                .collect()
+        })
+        .collect()
 }
 
-fn count_north_shifted_rounds(platform: Platform) -> usize {
-    fn add_rocks(round_count: usize, idx: usize) -> usize {
-        (0..round_count).into_iter().fold(0, |acc, i| acc + (idx - i))
+fn shift_and_rotate(platform: &mut Platform) {
+    let mut shifted: Platform = vec![];
+
+    let len = platform.len();
+    for x in 0..len {
+        let mut row = vec![];
+        let mut empties = vec![];
+        let mut rounds = vec![];
+
+        for y in 1..=len {
+            let rock = platform[len - y][x];
+            match rock {
+                Rock::Round => rounds.push(rock),
+                Rock::None => empties.push(rock),
+                Rock::Cube => {
+                    row.append(&mut empties);
+                    row.append(&mut rounds);
+                    row.push(rock);
+                    empties = vec![];
+                    rounds = vec![];
+                }
+            }
+        }
+        row.append(&mut empties);
+        row.append(&mut rounds);
+        shifted.push(row);
     }
 
-    platform.iter().fold(0, |acc, column: &Vec<Rock>| {
-        let mut row_score = 0;
-        let mut round_count = 0;
+    *platform = shifted
+}
 
-        for (idx, &rock) in column.iter().rev().enumerate() {
-            match rock {
-                Rock::Round => round_count += 1,
-                Rock::Cube => {
-                    row_score += add_rocks(round_count, idx);
-                    round_count = 0
-                },
-                _ => {}
-            };
-        };
+fn count_north_weight(platform: &Platform) -> usize {
+    platform
+        .iter()
+        .rev()
+        .enumerate()
+        .fold(0, |acc, (idx, column)| {
+            let mut row_score = 0;
 
-        row_score += add_rocks(round_count, column.len());
+            for &rock in column.iter() {
+                match rock {
+                    Rock::Round => row_score += idx + 1,
+                    _ => {}
+                };
+            }
 
-        acc + row_score
-    })
+            acc + row_score
+        })
+}
+
+fn hash_platform(platform: &Platform) -> u64 {
+    let mut hasher = DefaultHasher::new();
+    platform.hash(&mut hasher);
+    hasher.finish()
 }
 
 fn main() {
@@ -62,10 +95,26 @@ fn main() {
     let filename = &args[1];
     let document =
         std::fs::read_to_string(filename).expect("Something went wrong reading the file");
-    
-    let platform = parse_platform(&document);
-    let result = count_north_shifted_rounds(platform);
-    
+
+    let mut loop_detector: HashMap<u64, u32> = HashMap::new();
+
+    let mut platform = parse_platform(&document);
+    let mut i = 0;
+    let mut rotations_left = 1_000_000_000_u32;
+    while rotations_left > 0 {
+        for _ in 0..4 {
+            shift_and_rotate(&mut platform)
+        }
+        rotations_left -= 1;
+        i += 1;
+
+        if let Some(original_idx) = loop_detector.get(&hash_platform(&platform)) {
+            rotations_left = (1_000_000_000_u32 - i) % (i - original_idx);
+        }
+        loop_detector.insert(hash_platform(&platform), i);
+    }
+    let result = count_north_weight(&platform);
+
     println!("Result: {}", result);
 
     println!("Runtime: {:?}", now.elapsed());
